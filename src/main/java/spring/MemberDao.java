29ec835e8591512203e917a26cf85a031dfa2a2e
eeun <eeun95@gmail.com>
2022-08-08 17:30:27 +0900

    DB 연동

---
+++ src/main/java/spring/MemberDao.java
@@ -0,0 +1,25 @@
+package spring;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
+public class MemberDao {
+
+    public Member selectByEmail(String mail) {
+        return null;
+    }
+
+    public void insert(Member member) {
+
+    }
+
+    public void update(Member member) {
+
+    }
+
+    public Collection<Member> selectAll() {
+        return null;
+    }
+}
---
+++ src/main/java/spring/MemberListPrinter.java
@@ -0,0 +1,31 @@
+package spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component("listPrinter")
+public class MemberListPrinter {
+
+    private MemberDao memberDao;
+    private MemberPrinter printer;
+
+    public MemberListPrinter() {
+    }
+
+    public void printAll() {
+        Collection<Member> members = memberDao.selectAll();
+        members.forEach(m -> printer.print(m));
+    }
+
+    @Autowired
+    public void setMemberDao(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
+    @Autowired
+    public void setPrinter(MemberSummaryPrinter printer) {
+        this.printer = printer;
+    }
+}
